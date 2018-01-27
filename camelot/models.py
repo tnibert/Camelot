71fef9c59003769ed0b8c5dde834d3789dbc8680
Tim Nibert <docz2a@gmail.com>
2018-01-27 11:43:14 +0000

    fenegelled models and added app to project

--- camelot/models.py
+++ camelot/models.py
@@ -1,20 +1,14 @@
 from django.db import models
 
-class Album(models.Model):
-    name = models.CharField(max_length=70)
-    description = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('date published')
-    # contributors
-    owner = models.ForeignKey(User)
-    # groups - we'll need to check that these are only groups owned by our contributors
-
-    def __str__(self):
-        return self.name
-
 # perhaps the way to handle plural is a one to many such as
 # have a contributor model that just has foreign keys of user
 # and album...
 # or, we could use postgres arrays?
+
+# one to one relationship with User    
+class Profile(models.Model):
+    description = models.CharField(max_length=1000)
+    # albums
 
 class User(models.Model):
     # friends
@@ -23,10 +17,16 @@ class User(models.Model):
     # password
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-# one to one relationship with User    
-class Profile(models.Model):
-    description = models.CharField(max_length=1000)
-    # albums
+class Album(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('date published')
+    # contributors
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # groups - we'll need to check that these are only groups owned by our contributors
+
+    def __str__(self):
+        return self.name
 
 class Photo(models.Model):
     filename = models.CharField(max_length=200)
