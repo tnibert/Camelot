a0993193f9d06439e1820974623b69ea2282f6a2
Tim Nibert <docz2a@gmail.com>
2018-01-28 10:07:50 +0000

    successful user login, still broken unit test

--- camelot/tests.py
+++ camelot/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+class LoginTests(TestCase):
+    # well this is broken af        
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        u = User.objects.create_user(**self.credentials)
+        u.save()
+        
+    def test_login(self):
+        # send login data
+        response = self.client.post('', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_authenticated)
--- camelot/views.py
+++ camelot/views.py
@@ -4,6 +4,17 @@ from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     if request.method == "POST":
-        return HttpResponse("We have received a post!")
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse("Successful")
+            else:
+                return HttpResponse("Account Disabled")
+        else:
+            return HttpResponse("Invalid")
+
     else:
         return render(request, 'camelot/index.html')
