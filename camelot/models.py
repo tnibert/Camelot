d74245c331af75cadbba2531b1578af274517f80
Tim Nibert <docz2a@gmail.com>
2018-02-03 04:30:58 +0000

    updated models, reset db

--- camelot/models.py
+++ camelot/models.py
@@ -2,28 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # perhaps the way to handle plural is a one to many such as
-# have a contributor model that just has foreign keys of user
+# have a contributor model that just has foreign keys of profile
 # and album...
 # or, we could use postgres arrays?
+
+# for the moment
+# 1. register a user
+# 2. create an album
+# 3. upload images to that album
+# will take care of friends after that
 
 # one to one relationship with User    
 class Profile(models.Model):
     description = models.CharField(max_length=1000)
-    # albums
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class AppUser(User):
+# we don't need this, use django user for auth and profile composed with user
+#class AppUser(User):
     # friends
     # friendgroups
     # username = models.CharField(max_length=20)
     # password
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    #profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 class Album(models.Model):
     name = models.CharField(max_length=70)
     description = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date published')
     # contributors
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     # groups - we'll need to check that these are only groups owned by our contributors
 
     def __str__(self):
