5133062d13028ac71131861cfed423c8fe863f5f
Tim Nibert <docz2a@gmail.com>
2018-02-04 19:56:58 +1100

    added unit test for create album view

--- camelot/tests.py
+++ camelot/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+from django.shortcuts import reverse
+
 from django.contrib.auth.models import User
 
 class LoginTests(TestCase):
@@ -23,16 +26,35 @@ class LoginTests(TestCase):
         pass
 
 from .controllers import albumcontroller
+from .album import *
 
 class AlbumTests(TestCase):
+    # this setUp code needs to be made universal
     def setUp(self):
-        # create a user
-        pass
+        self.credentials = {
+            'username': 'testuser',
+            'email': 'user@test.com',
+            'password': 'secret'}
+        self.u = User.objects.create_user(**self.credentials)
+        self.u.save()
+
+        # send login data
+        #response = self.client.post('', self.credentials, follow=True)
+
+        self.factory = RequestFactory()
 
     def test_create_controller(self):
         pass
 
     def test_create_view(self):
-        pass
+        # Create an instance of a GET request.
+        request = self.factory.get(reverse("create_album"))
+        request.user = self.u
+        request.session = {}
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = create_album(request)
+
+        self.assertEqual(response.status_code, 200)
 
 # we need to go to each page in urls.py and check the http response code under all conditions
