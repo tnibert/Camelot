72faabc5d2121bc109ad4e485d1a5a9344716940
Tim Nibert <docz2a@gmail.com>
2018-02-04 20:19:34 +1100

    more unit testing for login, needs some more work

--- camelot/tests.py
+++ camelot/tests.py
@@ -4,19 +4,33 @@ from django.shortcuts import reverse
 
 from django.contrib.auth.models import User
 
+from .view.usermgmt import *
+
 class LoginTests(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'testuser',
             'email': 'user@test.com',
             'password': 'secret'}
-        u = User.objects.create_user(**self.credentials)
-        u.save()
+        self.u = User.objects.create_user(**self.credentials)
+        self.u.save()
+
+        self.factory = RequestFactory()
 
     def test_login(self):
         # send login data
         response = self.client.post('', self.credentials, follow=True)
         # should be logged in now
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_login_view_post(self):
+        request = self.factory.post(reverse("index"), data=self.credentials)
+        request.session = self.client.session
+
+        response = index(request)
+
+        # response 302 again... hmm
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_authenticated)
 
     def test_logout(self):
