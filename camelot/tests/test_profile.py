aace749bb91dadf5273a8849e98510bde9a42324
Tim Nibert <docz2a@gmail.com>
2018-03-19 20:09:28 +1100

    added view test for profile

--- camelot/tests/test_profile.py
+++ camelot/tests/test_profile.py
@@ -1,9 +1,23 @@
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 class ProfileControllerTests(TestCase):
     def setUp(self):
-        pass
+        self.credentials = {
+            'username': 'testuser',
+            'email': 'user@test.com',
+            'password': 'secret'}
+        self.u = User.objects.create_user(**self.credentials)
+        self.u.save()
+
+        self.credentials = {
+            'username': 'testuser2',
+            'email': 'user2@test.com',
+            'password': 'secret'}
+        self.u2 = User.objects.create_user(**self.credentials)
+        self.u2.save()
 
     def test_return_profile_data(self):
         pass
@@ -11,12 +25,34 @@ class ProfileControllerTests(TestCase):
     def test_update_profile_data(self):
         pass
 
+from ..view.profile import *
+
 class ProfileViewTests(TestCase):
     def setUp(self):
-        pass
+        # create user
+        self.credentials = {
+            'username': 'testuser',
+            'email': 'user@test.com',
+            'password': 'secret'}
+        self.u = User.objects.create_user(**self.credentials)
+        self.u.save()
+
+        # send login data
+        response = self.client.post('', self.credentials, follow=True)
+
+        self.factory = RequestFactory()
 
     def test_show_profile(self):
-        pass
+        # Create an instance of a GET request.
+        request = self.factory.get(reverse("show_profile", kwargs={'userid': self.u.id}))
+        request.user = self.u
+        request.session = {}
+
+        response = show_profile(request, self.u.id)
+
+        self.assertEqual(response.status_code, 200)
+
+        # test with another user's id
 
     def test_update_profile_view(self):
         pass
