8bf88e567f69e3b33873a195189e73a46500751f
Tim <docz2a@gmail.com>
2018-03-20 09:16:32 +1100

    scaffolding for permissions tests

---
+++ camelot/tests/test_permissions.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+"""
+In this file we need to define what our access permissions need to be
+and test correct access and access violations
+"""
+
+class test_controller_permissions(TestCase):
+    pass
+
+class test_view_permissions(TestCase):
+    pass
