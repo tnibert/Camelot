e080e4077cc2bb3f16af60b74eba0bb0b8d9a39f
Tim <docz2a@gmail.com>
2018-03-30 12:18:50 +1100

    added test helper function to add friends

--- camelot/tests/helperfunctions.py
+++ camelot/tests/helperfunctions.py
@@ -1,1 +1,14 @@
+from ..controllers.friendcontroller import friendcontroller
+
 # add friends
+def complete_add_friends(requesterid, requesteeid):
+    """
+
+    :param requesterid:
+    :param requesteeid:
+    :return: friendship object
+    """
+    friendcontrol1 = friendcontroller(requesterid)
+    friendcontrol2 = friendcontroller(requesteeid)
+    friendcontrol1.add(friendcontrol2.profile)
+    friendcontrol2.confirm(friendcontrol1.profile)
