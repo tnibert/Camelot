80f0ebf3dd3033834304bac36aee8a91f7d26c59
Tim Nibert <docz2a@gmail.com>
2018-04-01 11:06:30 +1000

    added notes about testing

--- camelot/tests/test_permissions.py
+++ camelot/tests/test_permissions.py
@@ -12,7 +12,7 @@ groups - only group members can view
 private - only owner and contributors can view
 
 Album:
-If album has no groups, default is all friends.
+If album has no groups, default is all friends? - not current implementation
 Contributor can always view album
 
 To validate an album:
@@ -22,5 +22,28 @@ Check if accessing user is in album's groups
 class test_controller_permissions(TestCase):
     pass
 
-class test_view_permissions(TestCase):
+class test_album_view_permissions(TestCase):
+    """
+    This test will go through all possible access cases
+    - not logged in
+        - can view public album
+    - logged in not friend
+        - can view public, album
+    - logged in friend not in group
+        - can view public, all friends album
+    - logged in friend in group
+        - can view public, all friends, groups album
+    - logged in contributor
+        - can view all access types
+        - can view manage page
+        - can add/remove own groups if access type is groups
+        - can add photos to album
+    - logged in owner
+        - can view all access types
+        - can view manage page
+        - can edit album access type
+        - can add/remove own groups
+        - can add/remove contributors
+        - can add photos to album
+    """
     pass
