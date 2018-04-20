985e8838af190ec0fa1cf2ea9b8cb55761f66d9b
Tim <docz2a@gmail.com>
2018-04-20 15:12:12 +1000

    moved pretty_request() to move appropriate location

--- camelot/tests/helperfunctions.py
+++ camelot/tests/helperfunctions.py
@@ -13,3 +13,26 @@ def complete_add_friends(requesterid, requesteeid):
     fship = friendcontrol1.add(get_profile_from_uid(requesteeid))
     friendcontrol2.confirm(get_profile_from_uid(requesterid))
     return fship
+
+
+def pretty_request(request):
+    headers = ''
+    for header, value in request.META.items():
+        if not header.startswith('HTTP'):
+            continue
+        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+        headers += '{}: {}\n'.format(header, value)
+
+    return (
+        '{method} HTTP/1.1\n'
+        'Content-Length: {content_length}\n'
+        'Content-Type: {content_type}\n'
+        '{headers}\n\n'
+        '{body}'
+    ).format(
+        method=request.method,
+        content_length=request.META['CONTENT_LENGTH'],
+        content_type=request.META['CONTENT_TYPE'],
+        headers=headers,
+        body=request.body,
+    )
--- camelot/view/album.py
+++ camelot/view/album.py
@@ -100,27 +100,6 @@ def display_photo(request, photoid):
 
     return render(request, 'camelot/presentphoto.html', retdict)
 
-def pretty_request(request):
-    headers = ''
-    for header, value in request.META.items():
-        if not header.startswith('HTTP'):
-            continue
-        header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
-        headers += '{}: {}\n'.format(header, value)
-
-    return (
-        '{method} HTTP/1.1\n'
-        'Content-Length: {content_length}\n'
-        'Content-Type: {content_type}\n'
-        '{headers}\n\n'
-        '{body}'
-    ).format(
-        method=request.method,
-        content_length=request.META['CONTENT_LENGTH'],
-        content_type=request.META['CONTENT_TYPE'],
-        headers=headers,
-        body=request.body,
-    )
 
 @login_required
 def add_photo(request, id):
