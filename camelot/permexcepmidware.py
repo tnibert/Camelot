13e21edc358d36667b034ba541c1f8afa1e00567
Tim Nibert <docz2a@gmail.com>
2018-05-07 20:06:38 +1000

    added middleware to catch permission exception #51

---
+++ camelot/permexcepmidware.py
@@ -0,0 +1,21 @@
+from .controllers.utilities import PermissionException, AlreadyExistsException
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib import messages
+from django.utils.deprecation import MiddlewareMixin
+
+class RedirectToRefererResponse(HttpResponseRedirect):
+    def __init__(self, request, *args, **kwargs):
+        redirect_to = request.META.get('HTTP_REFERER', '/')
+        super(RedirectToRefererResponse, self).__init__(
+            redirect_to, *args, **kwargs)
+
+class HandleBusinessExceptionMiddleware(MiddlewareMixin):
+    def process_exception(self, request, exception):
+        if isinstance(exception, PermissionException):
+            return render(request, 'camelot/permissionexception.html')
+        elif isinstance(exception, AlreadyExistsException):
+            #message = "Invalid operation %s" % unicode(exception)
+            #messages.error(request, message)
+            #return RedirectToRefererResponse(request)
+            pass
