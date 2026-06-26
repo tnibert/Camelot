012d2d90f3b1fbc257d7d901bd566f6793f8da40
Timothy Nibert <docz2a@gmail.com>
2026-06-26 21:08:30 +1000

    ensure ecs is updated to the latest docker image

--- ecs_service_setup.sh
+++ ecs_service_setup.sh
@@ -6,10 +6,11 @@ set -x
 
 # need AWS Session Manager dependency
 
-# wait for deployment to complete
+# wait for deployment to complete, force update the ecs deployment to latest
+aws ecs wait services-stable --cluster prod --services prod-backend-web
+aws ecs update-service --cluster prod --service prod-backend-web --force-new-deployment --region "$REGION"
 aws ecs wait services-stable --cluster prod --services prod-backend-web
 
 TASK_ID=$(aws ecs list-tasks --cluster prod --service-name prod-backend-web  --query 'taskArns[0]' --output text  | awk '{split($0,a,"/"); print a[3]}')
-#aws ecs update-service --cluster prod --service prod-backend-web --force-new-deployment --region "$REGION"
 aws ecs execute-command --task $TASK_ID --command "python manage.py collectstatic --noinput" --interactive --cluster prod --region $REGION
 aws ecs execute-command --task $TASK_ID --command "python manage.py migrate" --interactive --cluster prod --region $REGION
